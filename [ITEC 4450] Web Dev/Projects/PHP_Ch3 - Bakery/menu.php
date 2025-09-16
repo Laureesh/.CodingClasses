637926306a78c148ae7fd1674038f95c14906c9e
Laureesh Volmar <lvolmar1@ggc.edu>
2025-09-15 21:32:31 -0400

    Ch3 9/15/2025

---
+++ [ITEC 4450] Web Dev/Projects/PHP_Ch3 - Bakery/menu.php
@@ -0,0 +1,6 @@
+<div class="w3-bar w3-light-grey">
+  <a href="index.php" class="w3-bar-item w3-button">Home</a>
+  <a href="about.php" class="w3-bar-item w3-button">About Us</a>
+  <a href="menu.php" class="w3-bar-item w3-button">Menu</a>
+  <a href="contact.php" class="w3-bar-item w3-button">Contact Us</a>
+</div>
